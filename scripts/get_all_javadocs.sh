239ae3056f67c2a4a58b8bd8be33298ea9fe4a21
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-09-16 17:59:22 +0200

    Fix path

--- scripts/get_all_javadocs.sh
+++ scripts/get_all_javadocs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # This script gets all latest javadocs (SNAPSHOT)
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-bash ${__dir}/get_javadoc.sh keyple-java-core
-bash ${__dir}/get_javadoc.sh keyple-java-calypso
-bash ${__dir}/get_javadoc.sh keyple-java-plugin-pcsc
-bash ${__dir}/get_javadoc.sh keyple-java-plugin-remotese
-bash ${__dir}/get_javadoc.sh keyple-java-plugin-stub
-bash ${__dir}/get_javadoc.sh keyple-java-plugin-remotese
-bash ${__dir}/get_javadoc.sh keyple-android-plugin-nfc
-bash ${__dir}/get_javadoc.sh keyple-android-plugin-omapi
+
+./scripts/get_javadoc keyple-java-core
+./scripts/get_javadoc keyple-java-calypso
+./scripts/get_javadoc keyple-java-plugin-pcsc
+./scripts/get_javadoc keyple-java-plugin-remotese
+./scripts/get_javadoc keyple-java-plugin-stub
+./scripts/get_javadoc keyple-java-plugin-remotese
+./scripts/get_javadoc keyple-android-plugin-nfc
+./scripts/get_javadoc keyple-android-plugin-omapi
 
