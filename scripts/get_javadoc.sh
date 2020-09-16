15d807d41f8bf85efaa9afdbc38e1867de960890
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-09-16 18:07:22 +0200

    Fix path #4

--- scripts/get_javadoc.sh
+++ scripts/get_javadoc.sh
@@ -12,4 +12,4 @@ LATEST_UPLOADED_SNAPSHOT=$(wget -O - -o $BASE_URL/$1/$LATEST_SNAPSHOT/maven-meta
 # get the Javadoc jar
 wget $BASE_URL/$1/$LATEST_SNAPSHOT/$1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar"
 
-unzip $1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar" -d ./reference/$1
+unzip $1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar" -d reference/$1
