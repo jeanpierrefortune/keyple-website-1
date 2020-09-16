b84f30555040e48cb25c6a649c94d0cede9e7ab7
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-09-16 18:12:25 +0200

    Fix path #6, remove javadoc

--- scripts/get_javadoc.sh
+++ scripts/get_javadoc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ls -lrt
-
 # This script gets the latest javadoc JAR (SNAPSHOT) for the module provided in argument (e.g. keyple-java-core)
 
 # get the lastest SNAPSHOT version, put it in a variable named LATEST_SNAPSHOT_VERSION
@@ -14,4 +12,4 @@ LATEST_UPLOADED_SNAPSHOT=$(wget -O - -o $BASE_URL/$1/$LATEST_SNAPSHOT/maven-meta
 # get the Javadoc jar
 wget $BASE_URL/$1/$LATEST_SNAPSHOT/$1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar"
 
-unzip $1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar" -d reference/$1
+unzip $1"-"$LATEST_UPLOADED_SNAPSHOT"-javadoc.jar" -d public/reference/$1
